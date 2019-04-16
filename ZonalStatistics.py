751b3d48e2e63e8868513c8ce4636ad959994ace
Michael Mann <mmann1123@gmail.com>
2019-04-16 11:17:34 -0400

    updates

--- ZonalStatistics.py
+++ ZonalStatistics.py
@@ -9,49 +9,51 @@ import os
 from rasterstats import zonal_stats as zs
 import geopandas as gpd
 import pandas as pd
+import rasterio
+from glob import glob
 
+os.chdir(r'C:\Users\mmann\Dropbox\Belize\Geofiles\Time Series Properties')
 
 #Read the shapefile
-shp = gpd.GeoDataFrame.from_file("../zonal_stats/GN_Divisions.shp")
+shp = gpd.GeoDataFrame.from_file("../ED_2018_IDB_Shapefiles/ED_2018.shp")
+
+in_path = r'.'
 
 
+#%%
 # Save the images in one folder and create
 # a dictionary to read the files
 
-dirs = {
-  'fourier_sc31_mean': 'Sri_Lanka_fourier_bands/',
-  'fourier_sc31_variance': 'Sri_Lanka_fourier_bands/',
-  'fourier_sc51_mean': 'Sri_Lanka_fourier_bands/',
-  'fourier_sc51_variance': 'Sri_Lanka_fourier_bands/',
-  'fourier_sc71_mean': 'Sri_Lanka_fourier_bands/',
-  'fourier_sc71_variance': 'Sri_Lanka_fourier_bands/'
-}
 
+rasters = {}
+
+# add files to dictionary
+for entry in glob(r'./*.tif'):
+    name = os.path.basename(entry).split('.')[0]
+    rasters[name] = entry
+    
+    
 # select stats
 metrics = "sum mean std".split()
 
-# drop geometry from shapefile to save space
+# drop geometry from shapefile to save space in output
 shp_cols =shp.drop(['geometry'],axis=1)
 
-# Read files
-rasters = {}
-
-for n, d in dirs.items():
-        raster = (os.path.join(d, n+".tif"))
-        rasters[n]= raster
-
-
+ 
 # copy metrics
 spfeas_stats = shp_cols.copy()
+
 
 # calculate
 for rast, path in rasters.items():
 
-    stats = zs(shp, path, stats=metrics)
+    shp = shp.to_crs(rasterio.open(path).crs)
+    
+    stats = zs(shp, path, stats=metrics,)
     new_colnames = ["{}_{}".format(rast, metric) for metric in metrics]
     df = pd.DataFrame(stats)
     df2 = df.rename(columns=dict(zip(metrics, new_colnames)))
     spfeas_stats =spfeas_stats.join(df2)
 
 # Save
-spfeas_stats.to_csv("fourier_stats_all.csv")
+spfeas_stats.to_csv("./stats_all.csv")
