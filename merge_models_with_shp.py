64f7d50d16e4247034e74100fd117bc540213482
Michael Mann <mmann1123@gmail.com>
2020-04-14 17:56:42 -0400

    working

--- merge_models_with_shp.py
+++ merge_models_with_shp.py
@@ -17,19 +17,25 @@ import numpy as np
 os.chdir(r'E:\Dropbox\Belize\Writeup\Map Templates')
 
 #Read the shapefile
-shp = gpd.GeoDataFrame.from_file("./CensusEDPreds.shp")
+shp = gpd.read_file("./CensusEDPreds.shp")
+shp.crs.to_epsg()
 shp = shp[['FIPS','fips_1','X','geometry']]
 
 #New models
 models = pd.read_csv('./CensusEDPreds_GroupCV.csv', index_col=0)
 
 # exponentiate income columns
-models[['Ridge_Preds','ENet_Preds','RF_Preds','XGB_Preds','Preds_Combined']].apply(np.exp,axis=1, inplace=True)
+models[['Ridge_Preds','ENet_Preds','RF_Preds','XGB_Preds','Preds_Combined']] = models[['Ridge_Preds','ENet_Preds','RF_Preds','XGB_Preds','Preds_Combined']].apply(np.exp,axis=1)
 
 # merge in models
 shp = shp.astype({'FIPS': 'int64'})
 shp = shp.merge(models, left_on='FIPS', right_on='fips', how='left' )
+shp.crs = "EPSG:32616"
 
-#%%
+shp.rename(columns={'Poor_Combined_RPR20': "RPR20", 'Poor_Combined_RPR15': "RPR15", 
+                 'Poor_Combined_RPR10': "RPR10",'Poor_Combined_RPR05': "RPR05",
+                 'Preds_Combined':"RPRComb"}, inplace=True, errors="raise")
 
 shp.to_file("censusEDPreds_GroupCV.shp")
+
+#%%
